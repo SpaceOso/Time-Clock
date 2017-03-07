d3039341eccaf5a041c34257dfa5f1cec9882718
Miguel Rico <miguelricodev@gmail.com>
2017-03-07 07:42:34 -0500

    working on the math part

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const startGroup = document.getElementById('start-time-group');
 const endGroup = document.getElementById('end-time-group');
 
-
 //inputs
 let taskNameInput = document.getElementById('task-input');
 let startTimeHourInput = document.getElementById('start-time-hour');
@@ -10,10 +9,16 @@ let startTimeMinuteInput = document.getElementById('start-time-minutes');
 let endTimeHourInput = document.getElementById('end-time-hour');
 let endTimeMinuteInput = document.getElementById('end-time-minutes');
 
+//error message holder
+const errorParent = document.getElementById('error-message');
+
 //time values
 let taskName;
-let startTimeHour, startTimeMinutes;
-let endTimeHour, endTimeMinutes;
+
+//hours * 60
+let startHourInMinutes, endHourInMinutes;
+//startHourInMinutes + minutes;
+let totalStartMinutes, totalEndMinutes;
 
 let totalTasks = 0;
 let timeCollections = [];
@@ -21,6 +26,8 @@ let timeCollections = [];
 let errorText = {
     notANumber: "Please inter a valid integer",
     notAllForms: "Please enter a valid time into the remaining boxes",
+    higherStartTime: "Start time should be less than end time",
+    sameTimes: "Times can't be the same"
 };
 
 function checkForNumber(number) {
@@ -29,7 +36,6 @@ function checkForNumber(number) {
 
 function checkGroups(objecTimes) {
 
-    //destructering the object
     const {hour, minutes} = objecTimes;
 
     //check that the values have content
@@ -42,6 +48,11 @@ function checkGroups(objecTimes) {
         return false;
     }
 }
+
+function displayErrorMessage(errorMessage) {
+    errorParent.innerHTML = errorMessage;
+}
+
 
 function throwError(objectToError, addClass) {
     if (addClass) {
@@ -75,15 +86,17 @@ function setAMPM() {
 
     let settingsAMPM = {};
 
+    //check to see if the pm option is checked for the start time
     for(let i = 0; i < startAMPMInputs.length; i++){
         if(startAMPMInputs[i].checked == true){
-            settingsAMPM.startTime = startAMPMInputs[i].value;
+            settingsAMPM.startPM = startAMPMInputs[i].value == 'start-pm';
         }
     }
 
+    //check to see if the pm option is checked for the end time
     for(let i = 0; i < endAMPMInputs.length; i++){
         if(endAMPMInputs[i].checked == true){
-            settingsAMPM.endTime = endAMPMInputs[i].value;
+            settingsAMPM.endPM = endAMPMInputs[i].value == 'end-pm';
         }
     }
 
@@ -101,101 +114,88 @@ function displayTimes(savedTimes, taskCount) {
     let endTimeSelector = document.querySelector(`#end-time-${taskCount}`);
     let totalTimeSelector = document.querySelector(`#total-time-${taskCount}`);
 
-    let totalMinutes = (savedTimes.startTimes.minutes + savedTimes.endTimes.minutes);
-    let remainingMinutes = 0;
+    let totalHours = Math.trunc((totalEndMinutes - totalStartMinutes) / 60);
+    let totalMinutes = (totalEndMinutes - totalStartMinutes) % 60;
 
-    if (totalMinutes > 60) {
-        remainingMinutes = totalMinutes - 60;
-    } else {
-        remainingMinutes = totalMinutes;
+    console.log('totalHours: ', totalHours);
+
+    if(totalHours < 1){
+        totalHours = 0;
     }
-
-    let hourFromMinutes = Math.floor(totalMinutes / 60);
-    //TODO this is wrong, if the start time was 8:30 and end time was 9 it substracts 8 from 9 giving us an hour spent
-    let totalHours = savedTimes.endTimes.hour - savedTimes.startTimes.hour;
-    console.log(totalHours, remainingMinutes);
 
     taskNameSelector.innerHTML = `Task Name: ${savedTimes.taskName}`;
     startTimeSelector.innerHTML = `Start Time: ${savedTimes.startTimes.hour} : ${savedTimes.startTimes.minutes}`;
     endTimeSelector.innerHTML = `End Time: ${savedTimes.endTimes.hour} : ${savedTimes.endTimes.minutes}`;
-    totalTimeSelector.innerHTML = `Total Time: ${totalHours} : ${remainingMinutes}`;
+    totalTimeSelector.innerHTML = `Total Time: ${totalHours} : ${totalMinutes}`;
 
 }
 
 function confirmInputs(timeObject) {
-    //can't check for start time hour to be bigger than end time hour
-    //there could be the case when the start and end time both happened during the same hour
 
 
-    //TODO let minutes = minutes % 60;
-    //the above spits out the remaining minutes if you were to enter 90 minutes it will give you 30 back
     if(timeObject.startTimes.pm){
-       timeObject.startTimes.hour =  timeObject.startTimes.hour + 12;
+        if(timeObject.startTimes.hour < 12){
+            timeObject.startTimes.hour =  timeObject.startTimes.hour + 12;
+        }
+
     }
 
     if(timeObject.endTimes.pm){
-        timeObject.endTimes.hour = timeObject.endTimes.hour + 12;
-    }
-
-    //check if they are both within the same hour
-    if(timeObject.startTimes.hour == timeObject.endTimes.hour){
-        console.log("they both started at the same time");
-        //if they both started at the same hour we then need to check minutes
-        if(timeObject.startTimes.minutes < timeObject.endTimes.minutes){
-            console.log("we found out the difference based on the minutes");
-            return true;
+        if(timeObject.endTimes.hour < 12){
+            timeObject.endTimes.hour = timeObject.endTimes.hour + 12;
         }
     }
 
-    console.log("hours were not the same so we are just returning the input hours");
-    return timeObject.startTimes.hour < timeObject.startTimes.hour;
+    //convert hours to minutes
+    startHourInMinutes = timeObject.startTimes.hour * 60;
+    endHourInMinutes = timeObject.endTimes.hour * 60;
+
+    totalStartMinutes = startHourInMinutes + timeObject.startTimes.minutes;
+    totalEndMinutes = endHourInMinutes + timeObject.endTimes.minutes;
+
+
+    if(totalStartMinutes == totalEndMinutes){
+        displayErrorMessage(errorText.sameTimes);
+        return false;
+    }
+
+    if(totalStartMinutes < totalEndMinutes){
+        return true;
+    }
+
 }
 
 
 function grabValues() {
 
 
-    let ampmSettings = setAMPM();
-    let startPM, endPM;
+    displayErrorMessage('');
+
+    //returns an object with startTime and endTime AMPM values
+    let {startPM, endPM} = setAMPM();
 
     let firstGroupChecked, secondGroupChecked;
 
 
-    if(ampmSettings.startTime == 'start-pm'){
-        startPM = true;
-    }
-
-    if(ampmSettings.endTime == 'end-pm'){
-        endPM = true;
-    }
-
-
-    //grab task name
-    taskName = taskNameInput.value;
-    //convert inputs into numbers
-    startTimeHour = +startTimeHourInput.value;
-    startTimeMinutes = +startTimeMinuteInput.value;
-
-    endTimeHour = +endTimeHourInput.value;
-    endTimeMinutes = +endTimeMinuteInput.value;
-
-
     let currentTimes = {
-        taskName: taskName,
+        taskName: taskNameInput.value,
         startTimes: {
             pm: startPM,
-            hour: startTimeHour,
-            minutes: startTimeMinutes,
+            hour: +startTimeHourInput.value,
+            minutes: +startTimeMinuteInput.value,
         },
         endTimes: {
             pm: endPM,
-            hour: endTimeHour,
-            minutes: endTimeMinutes
+            hour: +endTimeHourInput.value,
+            minutes: +endTimeMinuteInput.value
         }
     };
 
     //need a function to check that the second time is not smaller than the first time
-    confirmInputs(currentTimes);
+    if(!confirmInputs(currentTimes)){
+        displayErrorMessage(errorText.higherStartTime);
+        return false;
+    }
 
 
     //check the first group for validation
